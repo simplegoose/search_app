d6faa8f80e5df3a9e2d2136a23c6ff80ffff52b7
Timothy Njoroge <timothynjoroge56@gmail.com>
2024-07-25 22:23:25 +0300

    apply hotfix to algorithm

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,21 +2,12 @@ class SearchesController < ApplicationController
   # GET /searches or /searches.json
   def index
     query = params[:query]
+    return if query.blank?
 
-    return unless query
+    @searches = find_searches(query)
+    increment_search_counts(@searches)
 
-    exists = check_existing
-
-    @searches = Search.where('lower(search_params) Like ?', "%#{query.downcase}%")
-
-    @searches.each do |search|
-      search.count = search.count + 1
-      search.save
-    end
-
-    return if exists
-
-    Search.create(ip_address: request.remote_ip, search_params: params[:query], count: 1)
+    create_search(query) unless search_exists?(query)
   end
 
   # GET /searches/1 or /searches/1.json
@@ -26,27 +17,47 @@ class SearchesController < ApplicationController
 
   private
 
-  def check_existing
-    query = params[:query]
+  def find_searches(query)
+    Search.where('lower(search_params) LIKE ?', "%#{query.downcase}%")
+  end
+
+  def increment_search_counts(searches)
+    searches.each { |search| search.increment!(:count) }
+  end
+
+  def search_exists?(query)
+    cleaned_query = split_query(query)
+
+    result = find_filtered_results(cleaned_query.first)
+
+    return false unless result
+
+    update_search_params_if_needed(result, query)
+
+    true
+  end
+
+  def split_query(query)
     cleaned_query = query[0...query.rindex(' ')].rstrip
+
+    return [query, query] if query.split.length < 2
+
     last_query_word = query.split.last
 
-    result = Search.where('lower(search_params) Like ?', "%#{cleaned_query.downcase}%")
-    first_of_result = result.find do |obj|
-      (last_query_word.include? obj.search_params.split.last) || (obj.search_params.last.include? last_query_word)
-    end
+    [cleaned_query, last_query_word]
+  end
 
-    return false unless first_of_result
+  def find_filtered_results(cleaned_query)
+    Search.find_by('lower(search_params) LIKE ?', "%#{cleaned_query.downcase}%")
+  end
 
-    if first_of_result &&
-       query.rstrip.length > first_of_result.search_params.length &&
-       first_of_result.ip_address == request.remote_ip
-      first_of_result.search_params = query
-      first_of_result.save
+  def update_search_params_if_needed(search, query)
+    return unless query.rstrip.length > search.search_params.rstrip.length
 
-      return result.length.positive?
-    end
+    search.update(search_params: query, ip_address: search.ip_address)
+  end
 
-    first_of_result
+  def create_search(query)
+    Search.create(ip_address: request.remote_ip, search_params: query, count: 1)
   end
 end
